87cdf9310ff0d372613342b63436171803061820
Max <max@linux-bds1.site>
2015-11-08 14:20:04 -0800

    Adding unit hierarchy filtering

--- ID_assigner.py
+++ ID_assigner.py
@@ -36,3 +36,53 @@ def ID_assigner(raw_IDs):
 
 
     return ID_list, ID_dict
+
+
+
+def unit_ID_assigner(raw_IDs):
+    """
+    Splits the problem hierarchy IDs into unuits and sections, assigns integers and stores in in separate dictionary
+    
+    Inputs
+    ------
+    raw IDs : list
+        list of strings of IDs
+        
+    Returns
+    -------
+    filtered IDs : list
+        Unit and Section list, but with integer IDs instead of strings
+
+    ID dict : dictionary
+	The raw IDs in a dictionary for quick lookup.
+
+    Usage: [hier_ID,unit_dict,sect_dict] = ID_assigner(xy_train['Problem Hierarchy'])
+    This function works under the assumption that 'Problem Hierarchy' strings
+    are in the form 'Unit XXX, Section XXX'
+    """
+    IDstr_Len = len(raw_IDs)
+    ID_list = np.empty((IDstr_Len,2),dtype=int)
+    unit_dict = defaultdict(list)
+    sect_dict = defaultdict(list)
+    unit_dlen = 0
+    sect_dlen = 0
+
+#    print IDstr_Len
+
+    for k in range(IDstr_Len):
+        curr_ID = str(raw_IDs[k]).split(', ')
+        if curr_ID[0] not in unit_dict:
+            unit_dict.update({curr_ID[0]: unit_dlen})
+            ID_list[k,0] = unit_dlen
+            unit_dlen = unit_dlen + 1
+        else:
+            ID_list[k,0] = unit_dict.get(curr_ID[0])
+
+        if curr_ID[1] not in sect_dict:
+            sect_dict.update({curr_ID[1]: sect_dlen})
+            ID_list[k,1] = sect_dlen
+            sect_dlen = sect_dlen + 1
+        else:
+            ID_list[k,1] = sect_dict.get(curr_ID[1])
+
+    return ID_list,unit_dict,sect_dict
