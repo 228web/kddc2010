b244b4fd42e34479f16c8778596b55ee57caeb47
Max <max@linux-bds1.site>
2015-11-08 13:56:43 -0800

    Just modifying...

--- ID_assigner.py
+++ ID_assigner.py
@@ -1,23 +1,24 @@
-def student_ID_assigner(student_IDs):
+def ID_assigner(raw_IDs):
     """
-    Converts student IDs to integers, stores IDs in separate dictionary
+    Converts the IDs to integers, stores in in separate dictionary
     
     Inputs
     ------
-    Student IDs : list
-        list of strings of anonymous student IDs
+    raw IDs : list
+        list of strings of IDs
         
     Returns
     -------
-    student IDs : list
+    filtered IDs : list
         The same list, but with integer IDs instead of strings
 
     ID dict : dictionary
-	The student IDs in a dictionary for quick lookup.
+	The raw IDs in a dictionary for quick lookup.
 
-    Usage: [stud_ID,stud_dict] = student_ID_assigner(xy_train['Anon Student Id']
+    Usage: [filt_ID,filt_dict] = ID_assigner(xy_train[STRING HERE])
+    Keys that need filtering: 'Anon Student Id','Problem Name'
     """
-    IDstr_Len = len(student_IDs)
+    IDstr_Len = len(raw_IDs)
     ID_list = np.empty(IDstr_Len,dtype=int)
     ID_dict = defaultdict(list)
     dict_len = 0
@@ -25,7 +26,7 @@ def student_ID_assigner(student_IDs):
 #    print IDstr_Len
 
     for k in range(IDstr_Len):
-        curr_ID = str(student_IDs[k])
+        curr_ID = str(raw_IDs[k])
         if curr_ID not in ID_dict:
             ID_dict.update({curr_ID: dict_len})
             ID_list[k] = dict_len
