cdee979e3318393f96f34938604399b62ace4ad9
JGLee6 <j.greendeer.lee@gmail.com>
2015-11-08 12:17:52 -0800

    Converts times to seconds since 1970 and checks if step is final answer.

--- loader.py
+++ loader.py
@@ -6,6 +6,7 @@ Created on Sat Nov 07 13:34:10 2015
 """
 
 import numpy as np
+import datetime as dt
 from collections import defaultdict
 
 trainDat = 'algebra_2005_2006_train.txt'
@@ -64,4 +65,69 @@ def loader(data):
             print line
             
     return keys, x
+    
+def convert_times(time):
+    """
+    Converts strings in training data to datetime structures and then to
+    seconds since Jan. 1, 1970.
+    
+    Inputs
+    ------
+    time : list
+        list of Time events in training data
+    
+    Returns
+    -------
+    t : ndarray
+        array of time events in seconds since Jan. 1, 1970. t[k] = 0 if entry
+        doesn't match format, likely blank.
+    """
+    t0 = dt.datetime(1970,1,1)
+    tLen = len(time)
+    t = np.empty(tLen)
+    for k in range(tLen):
+        try:
+            t1 = dt.datetime.strptime(time[k], '%Y-%m-%d %H:%M:%S.%f')
+            deltaT = (t1-t0).total_seconds()
+        except ValueError:
+            print "Error, doesn't match format: %Y-%m-%d %H:%M:%S.%f"
+            print 'Entry: '+str(k)+', Line: '+time[k]
+            deltaT = 0.0
+        
+        t[k] = deltaT
+        
+    return t
+
+def string_to_tag(data):
+    """
+    Convert strings to tag id numbers
+    """
+    
+    return 0
+
+def check_final_answer(stepName):
+    """
+    Check if string of 'Step Name' key is 'Final Answer' or not.
+    
+    Inputs
+    ------
+    stepName : list
+        list of strings of Step Name
+        
+    Returns
+    -------
+    finalAns : ndarray
+        1 if Final Answer, 0 otherwise
+    """
+    sNameLen = len(stepName)
+    finalAns = np.empty(sNameLen)
+    
+    for k in range(sNameLen):
+        if 'Final' in stepName[k]:
+            finalAns[k] = 1
+        else:
+            finalAns[k] = 0
+            
+    return finalAns
+
     
