86f693c7ab12fab4c811351a44d9d7ba76e7f341
Max <max@linux-bds1.site>
2015-11-11 17:11:12 -0800

    Fixing dictionary

--- ID_assigner.py
+++ ID_assigner.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 def ID_assigner(raw_IDs):
     """
     Converts the IDs to integers, stores in in separate dictionary
@@ -21,18 +23,21 @@ def ID_assigner(raw_IDs):
     IDstr_Len = len(raw_IDs)
     ID_list = np.empty(IDstr_Len,dtype=int)
     ID_dict = defaultdict(list)
-    dict_len = 0
 
+    dict_len = 1
+    ID_dict.update({0 : ''})
 #    print IDstr_Len
 
     for k in range(IDstr_Len):
         curr_ID = str(raw_IDs[k])
-        if curr_ID not in ID_dict:
-            ID_dict.update({curr_ID: dict_len})
+        if curr_ID == '':
+            ID_list[k] = 0
+        elif curr_ID not in ID_dict.values():
+            ID_dict.update({dict_len: curr_ID})
             ID_list[k] = dict_len
             dict_len = dict_len + 1
         else:
-            ID_list[k] = ID_dict.get(curr_ID)
+            ID_list[k] = ID_dict.values().index(curr_ID)
 
 
     return ID_list, ID_dict
@@ -64,25 +69,33 @@ def unit_ID_assigner(raw_IDs):
     ID_list = np.empty((IDstr_Len,2),dtype=int)
     unit_dict = defaultdict(list)
     sect_dict = defaultdict(list)
-    unit_dlen = 0
-    sect_dlen = 0
+
+    unit_dlen = 1
+    sect_dlen = 1
+    unit_dict.update({0 : ''})
+    sect_dict.update({0 : ''})
 
 #    print IDstr_Len
 
     for k in range(IDstr_Len):
         curr_ID = str(raw_IDs[k]).split(', ')
-        if curr_ID[0] not in unit_dict:
-            unit_dict.update({curr_ID[0]: unit_dlen})
+
+        if curr_ID[0] == '':
+            unit_list[k] = 0
+        elif curr_ID[0] not in unit_dict.values():
+            unit_dict.update({unit_dlen: curr_ID[0]})
             ID_list[k,0] = unit_dlen
             unit_dlen = unit_dlen + 1
         else:
-            ID_list[k,0] = unit_dict.get(curr_ID[0])
+            ID_list[k,0] = unit_dict.values().index(curr_ID[0])
 
-        if curr_ID[1] not in sect_dict:
-            sect_dict.update({curr_ID[1]: sect_dlen})
+        if curr_ID[1] == '':
+            sect_list[k] = 0
+        elif curr_ID[1] not in sect_dict.values():
+            sect_dict.update({sect_dlen: curr_ID[1]})
             ID_list[k,1] = sect_dlen
             sect_dlen = sect_dlen + 1
         else:
-            ID_list[k,1] = sect_dict.get(curr_ID[1])
+            ID_list[k,1] = sect_dict.values().index(curr_ID[1])
 
     return ID_list,unit_dict,sect_dict
--- data_processor.py
+++ data_processor.py
@@ -13,20 +13,44 @@ for i in range(4):
     xy_train[time_strings[i]] = convert_times(xy_train[time_strings[i]])
 
 id_strings = ['Anon Student Id','Problem Name']
+all_dicts = []
 
 for i in range(2):
     print 'Processing ' + id_strings[i]
-    xy_train[id_strings[i]] = ID_assigner(xy_train[id_strings[i]])
+    [xy_train[id_strings[i]],temp] = ID_assigner(xy_train[id_strings[i]])
+    all_dicts.append(temp)
 
-xy_train['Problem Hierarchy'] = unit_ID_assigner(xy_train['Problem Hierarchy'])
+[xy_train['Problem Hierarchy'],temp,temp2] = unit_ID_assigner(xy_train['Problem Hierarchy'])
+all_dicts.append(temp)
+all_dicts.append(temp2)
+
+#All_dicts:
+#	0: Student ID
+#	1: Problem Name
+#	2: Problem Unit
+#	3: Problem Section
 
 tag_master = string_tags(xy_train['KC(Default)'])
 
 [tag_array,opp_array] = tags_to_array(xy_train['KC(Default)'],xy_train['Opportunity(Default)'],tag_master)
 
-#Sanity check - do multiple skills correspond to multiple arrays?
-mult_skill_locs = np.where(np.sum(tag_array,1)>1)
+#Sanity check - do multiple skills correspond to multiple non-zero opportunities?
 
+
+for i in range(len(tag_array)):
+    if(np.count_nonzero(opp_array[i,:]) != np.sum(tag_array[i,:])):
+        print 'ERROR: Opp does not contain correct number of entries: ' + str(i)
+
+#Another sanity check - does opportunity increment for each instance of a skill FOR each student
+for i in range(len(tag_master)):
+    skill_locs = np.where(tag_array[:,i] > 0)
+    print [opp_array[j,i] for j in skill_locs]
+    a =  [opp_array[j,i] for j in skill_locs]
+
+#Look up location of index in array
+#tag_master.index(SOME STRING)
+
+#Write a decision tree
 
 # Calculate step start time from FIRST student transaction
 # Calculate the above, but separate by skill?
